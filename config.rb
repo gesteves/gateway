f5caaf97aa49ce7c92b805df202f4b8a2f54f01d
Guillermo Esteves <g@gesteves.com>
2019-02-04 18:08:18 -0700

    Set some configuration

--- config.rb
+++ config.rb
@@ -1,46 +1,45 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
+configure :development do
+  config[:protocol]      = 'http://'
+  config[:host]          = '0.0.0.0'
+  config[:port]          = 4567
+  config[:css_dir]       = 'stylesheets'
+  config[:js_dir]        = 'javascripts'
+  config[:images_dir]    = 'images'
+
+  activate :gzip
+  activate :dotenv
+  activate :autoprefixer do |config|
+    config.browsers = ['last 1 version', 'last 3 safari versions', 'last 3 ios versions']
+  end
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
+configure :production do
+  config[:protocol]      = 'https://'
+  config[:host]          = 'www.gesteves.com/'
+  config[:port]          = 80
+  config[:css_dir]       = 'stylesheets'
+  config[:js_dir]        = 'javascripts'
+  config[:images_dir]    = 'images'
 
-# Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+  activate :gzip
+  activate :dotenv
+  activate :autoprefixer do |config|
+    config.browsers = ['last 1 version', 'last 3 safari versions', 'last 3 ios versions']
+  end
+  activate :s3_sync do |s3|
+    s3.prefer_gzip           = true
+    s3.bucket                = ENV['AWS_BUCKET']
+    s3.region                = ENV['AWS_REGION']
+    s3.aws_access_key_id     = ENV['AWS_ACCESS_KEY_ID']
+    s3.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY_ID']
+  end
+  activate :minify_css
+  activate :asset_hash
+  activate :relative_assets
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+  caching_policy 'text/html',    :max_age => 86400, :must_revalidate => true
+  default_caching_policy         :max_age => 60 * 60 * 24 * 365
+end

2b5a0f93f15ed71c27c6e9c6143a09a68d67a0b3
Guillermo Esteves <g@gesteves.com>
2019-02-05 11:47:00 -0700

    Change photo layout

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -19,10 +19,10 @@ module CustomHelpers
   def denali_image_tag(photo)
     caption = photo.attributes.caption.nil? ? "Latest from my photoblog" : photo.attributes.caption
     photo_url = image_path "denali/#{photo.id}.jpg"
-    sizes_array = [116, 144, 245, 216, 274, 206, 412, 618].sort
+    sizes_array = [252, 307, 461, 519, 340, 274, 206, 412].sort.uniq
     srcset = srcset(photo_url, sizes_array)
     src = imgix_url(photo_url, { w: sizes_array.first })
-    sizes = "(min-width: 1440px) 206px, (min-width: 1024px) calc((((200vw/3) - 6rem)/4) - 10px), calc(((100vw - 3rem)/4) - 10px)"
+    sizes = "(min-width: 1440px) 206px, (min-width: 1024px) calc((((200vw/3) - 6rem)/4) - 10px), (min-width: 768px) calc(((100vw - 3rem)/4) - 10px), calc(((100vw - 3rem)/2) - 10px)"
     content_tag 'img', nil, intrinsicsize: "#{sizes_array.first}x#{sizes_array.first}", src: src, srcset: srcset, sizes: sizes, alt: caption
   end
 

355bf17253f3854982162b25858abd58a5bde612
Guillermo Esteves <g@gesteves.com>
2019-06-09 17:44:07 -0600

    Do a named query

--- lib/import/denali.rb
+++ lib/import/denali.rb
@@ -6,8 +6,8 @@ module Import
     HTTP = GraphQL::Client::HTTP.new('https://www.allencompassingtrip.com/graphql')
     Schema = GraphQL::Client.load_schema(HTTP)
     Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
-    RecentEntriesQuery = Client.parse <<-'GRAPHQL'
-      query ($count: Int) {
+    Queries = Client.parse <<-'GRAPHQL'
+      query RecentEntries($count: Int) {
         blog {
           name
           entries(count: $count) {
@@ -24,7 +24,7 @@ module Import
     GRAPHQL
 
     def self.get_photos
-      response = Client.query(RecentEntriesQuery, variables: { count: 4 })
+      response = Client.query(Queries::RecentEntries, variables: { count: 4 })
       response.data.blog.entries.map { |e| File.open("source/images/denali/#{e.id}.jpg",'w'){ |f| f << HTTParty.get(e.photos.first.thumbnail_urls.first).body } }
       File.open('data/denali.json','w'){ |f| f << response.data.to_h.to_json }
     end

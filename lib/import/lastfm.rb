aa0b4f7f748f5c3c20dfde0f58333e4a7bd0f8c2
Guillermo Esteves <g@gesteves.com>
2021-12-21 19:22:22 -0700

    Refactor last.fm

--- lib/import/lastfm.rb
+++ lib/import/lastfm.rb
@@ -23,7 +23,7 @@ require 'active_support/all'
       return if response.code >= 400
 
       tracks = JSON.parse(response.body).dig('toptracks', 'track')&.map do |t|
-        track = track(t['name'], t['artist']['name'])
+        track = track(t['mbid'], t['name'], t['artist']['name'])
         track[:play_count] = t['playcount'].to_i
         track
       end
@@ -31,20 +31,13 @@ require 'active_support/all'
       File.open('data/music.json','w'){ |f| f << tracks.compact.to_json }
     end
 
-    def track(track, artist)
-      query = {
-        method: 'track.getInfo',
-        api_key: @api_key,
-        format: 'json',
-        track: track,
-        artist: artist
-      }
-      response = HTTParty.get(@base_url, query: query)
-      return if response.code >= 400
+    def track(mbid, name, artist)
+      track = track_by_mbid(mbid) || track_by_name_and_artist(name, artist)
+      return if track.blank?
 
-      track = JSON.parse(response.body)['track']
       artist = artist(track.dig('artist', 'mbid'))
       return if artist.blank?
+
       album = album(track.dig('album', 'mbid'))
       return if album.blank?
 
@@ -53,8 +46,31 @@ require 'active_support/all'
       track
     end
 
+    def track_by_mbid(mbid)
+      query = {
+        method: 'track.getInfo',
+        api_key: @api_key,
+        format: 'json',
+        mbid: mbid
+      }
+
+      response = HTTParty.get(@base_url, query: query)
+      JSON.parse(response.body)['track']
+    end
+
+    def track_by_name_and_artist(name, artist)
+      query = {
+        method: 'track.getInfo',
+        api_key: @api_key,
+        format: 'json',
+        track: name,
+        artist: artist
+      }
+      response = HTTParty.get(@base_url, query: query)
+      JSON.parse(response.body)['track']
+    end
+
     def artist(mbid)
-      return if mbid.blank?
       query = {
         method: 'artist.getInfo',
         api_key: @api_key,
@@ -62,13 +78,10 @@ require 'active_support/all'
         mbid: mbid
       }
       response = HTTParty.get(@base_url, query: query)
-      return if response.code >= 400
-
       JSON.parse(response.body)['artist']
     end
 
     def album(mbid)
-      return if mbid.blank?
       query = {
         method: 'album.getInfo',
         api_key: @api_key,
@@ -76,8 +89,6 @@ require 'active_support/all'
         mbid: mbid
       }
       response = HTTParty.get(@base_url, query: query)
-      return if response.code >= 400
-
       JSON.parse(response.body)['album']
     end
   end

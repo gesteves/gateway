8993f5324dd6df438a69d03fa14b05a687ceff84
Guillermo Esteves <g@gesteves.com>
2022-01-07 11:38:37 -0700

    Simplify website

--- lib/import.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'import/denali'
-require_relative 'import/github'
-require_relative 'import/goodreads'
-require_relative 'import/letterboxd'
-require_relative 'import/lastfm'
-require_relative 'import/darksky'
-
-module Import
-end

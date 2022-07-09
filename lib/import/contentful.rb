2d522acebd3a451dd7c0d3b402331aa4072c2c18
Guillermo Esteves <g@gesteves.com>
2022-07-09 08:14:10 -0600

    Import content from Contentful

---
+++ lib/import/contentful.rb
@@ -0,0 +1,88 @@
+require 'graphql/client'
+require 'graphql/client/http'
+require 'dotenv'
+require 'rdoc'
+
+module Import
+  module Contentful
+    Dotenv.load
+    HTTP = GraphQL::Client::HTTP.new("https://graphql.contentful.com/content/v1/spaces/#{ENV['CONTENTFUL_SPACE']}")do
+      def headers(context)
+        { "Authorization": "Bearer #{ENV['CONTENTFUL_TOKEN']}" }
+      end
+    end
+    Schema = GraphQL::Client.load_schema(HTTP)
+    Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
+    Queries = Client.parse <<-'GRAPHQL'
+      query Content {
+        contentTypeEntryCollection(limit: 1000) {
+          items {
+            title
+            slug
+            body
+            author {
+              name
+            }
+            summary
+            published
+            indexInSearchEngines
+            sys {
+              firstPublishedAt
+              publishedAt
+            }
+          }
+        }
+        pageCollection(limit: 1000) {
+          items {
+            title
+            slug
+            body
+            description
+            sys {
+              firstPublishedAt
+              publishedAt
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    def self.content
+      response = Client.query(Queries::Content)
+
+      entries = response.data.content_type_entry_collection.items.map { |item| render_body(item) }.map { |item| set_timestamps(item) }.map { |item| set_entry_path(item) }
+      File.open('data/entries.json','w'){ |f| f << entries.to_json }
+
+      pages = response.data.page_collection.items.map { |item| render_body(item) }.map { |item| set_timestamps(item) }.map { |item| set_entry_path(item) }
+      File.open('data/pages.json','w'){ |f| f << pages.to_json }
+    end
+
+    def self.render_body(item)
+      formatter = RDoc::Markup::ToHtml.new(RDoc::Options.new, nil)
+      html = RDoc::Markdown.parse(item.body).accept(formatter)
+      item = item.to_h.dup
+      item[:html] = html
+      item
+    end
+
+    def self.set_entry_path(item)
+      item = item.dup
+      published = DateTime.parse(item[:published_at])
+      item[:path] = "/blog/#{published.strftime('%Y')}/#{published.strftime('%m')}/#{published.strftime('%d')}/#{item['slug']}/index.html"
+      item
+    end
+
+    def self.set_page_path(item)
+      item = item.dup
+      item[:path] = "/#{item['slug']}/index.html"
+      item
+    end
+
+    def self.set_timestamps(item)
+      item = item.dup
+      item[:published_at] = item.dig('published') || item.dig('sys', 'firstPublishedAt')
+      item[:updated_at] = item.dig('sys', 'publishedAt')
+      item
+    end
+  end
+end
